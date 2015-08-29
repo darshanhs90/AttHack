255564a98aa7db5c36d342f13c03b90143bd1f9c
Haridarshan <hsdars@gmail.com>
2015-08-29 18:23:45 -0500

    changes

--- public/js/index-angular.js
+++ public/js/index-angular.js
@@ -37,12 +37,11 @@ app.controller('myCtrl', function($scope, $http,$window) {
         }).success(function(data, status, headers, config) {
 
          $scope.eventList=(data.events);
-         console.log($scope.eventList[0]);
-         $scope.b=$scope.eventList[0].name;
+
            //$scope.txtarea=$scope.eventList;
-          //  for (var i = $scope.eventList - 1; i >= 0; i--) {
-          //     $scope.b+=$scope.eventList[i].name+",";
-          // };
+           for (var i = $scope.eventList - 1; i >= 0; i--) {
+              $scope.b+=$scope.eventList[i].name.text+",";
+          };
 
 
       });
